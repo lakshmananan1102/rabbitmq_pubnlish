77eaeebdd603b7a64c917d0f396b3429b2b2e967
lakshmananan1102 <103510774+lakshmananan1102@users.noreply.github.com>
2022-11-17 17:33:23 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,50 +1,50 @@
-import cv2
-import numpy as np
+
+from __future__ import absolute_import, unicode_literals
+import datetime
+
+from kombu import Connection
+from kombu import Exchange
+from kombu import Producer
+from kombu import Queue
+
 import sys
 import time
 
-from kombu import Connection, Exchange, Queue
-from kombu.mixins import ConsumerMixin
+import cv2
 
 # Default RabbitMQ server URI
 rabbit_url = 'amqp://guest:guest@localhost:5672//'
+
+# Kombu Connection
+conn = Connection(rabbit_url)
+channel = conn.channel()
+
+# Kombu Exchange
+# - set delivery_mode to transient to prevent disk writes for faster delivery
+exchange = Exchange("video-exchange", type="direct", delivery_mode=1)
+
+# Kombu Producer
+producer = Producer(exchange=exchange, channel=channel, routing_key="video")
+
+# Kombu Queue
+queue = Queue(name="video-queue", exchange=exchange, routing_key="video") 
+queue.maybe_bind(conn)
+queue.declare()
+
+# Video Capture by OpenCV
+capture = cv2.VideoCapture(0)
+encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
 import base64
-# Kombu Message Consuming Worker
-class Worker(ConsumerMixin):
-    def __init__(self, connection, queues):
-        self.connection = connection
-        self.queues = queues
+while True:
+    ret, frame = capture.read()
+    if ret is True:
+        # Make image smaller for faster delivery
+        frame = cv2.resize(frame, None, fx=0.6, fy=0.6)
+        # Encode into JPEG
+        result, imgencode = cv2.imencode('.jpg', frame, encode_param)
+        # Send JPEG-encoded byte array
+        jpgb64 = base64.b64encode(imgencode)
 
-    def get_consumers(self, Consumer, channel):
-        return [Consumer(queues=self.queues,
-                         callbacks=[self.on_message],
-                         accept=['image/jpeg'])]
 
-    def on_message(self, body, message):
-        # get the original jpeg byte array size
-        imgbin = base64.b64decode(body)
-        # # jpeg-encoded byte array into numpy array
-        # np_array = np.frombuffer(imgbin, dtype=np.uint8)
-        # print(np_array)
-        jpg = np.frombuffer(imgbin, dtype=np.uint8)
-
-        # JPEG-decode back into original frame - which is actually a Numpy array
-        im = cv2.imdecode(jpg, cv2.IMREAD_UNCHANGED)
-
-        # # decode jpeg-encoded numpy array 
-        # # show image
-        cv2.imshow("image", im)
-        cv2.waitKey(1)
-
-        # send message ack
-        message.ack()
-
-def run():
-    exchange = Exchange("video-exchange", type="direct")
-    queues = [Queue("video-queue", exchange, routing_key="video")]
-    with Connection(rabbit_url, heartbeat=4) as conn:
-            worker = Worker(conn, queues)
-            worker.run()
-
-if __name__ == "__main__":
-    run()
+        producer.publish(jpgb64, content_type='image/jpeg', content_encoding='binary')
+    time.sleep(0.001)
